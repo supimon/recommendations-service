e9e64fd878bcba0070f25e2cb7e679777469d1c2
Supi <supimon@gmail.com>
2020-01-02 18:13:49 +0530

    first commit

---
+++ src/main/java/com/supimon/recommendationsservice/RecommendationsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.supimon.recommendationsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendationsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommendationsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/supimon/recommendationsservice/RecommendationsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supimon.recommendationsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
