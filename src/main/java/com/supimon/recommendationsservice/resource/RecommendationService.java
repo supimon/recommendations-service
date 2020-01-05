5069dfae6f4589b10c094cccdc36975fdfc64054
Supi <supimon@gmail.com>
2020-01-05 13:07:42 +0530

    added firebase and initialised it

--- src/main/java/com/supimon/recommendationsservice/resource/RecommendationService.java
+++ src/main/java/com/supimon/recommendationsservice/resource/RecommendationService.java
@@ -1,10 +1,15 @@
 package com.supimon.recommendationsservice.resource;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import com.supimon.recommendationsservice.models.RecommendationModel;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
@@ -13,7 +18,18 @@ import java.util.List;
 public class RecommendationService {
 
     @RequestMapping("/{chefId}")
-    public int getRecommendations(@PathVariable("chefId") String chefId){
+    public int getRecommendations(@PathVariable("chefId") String chefId) throws IOException {
+
+        FileInputStream serviceAccount =
+                new FileInputStream("src/main/resources/chefapp-eeae0-firebase-adminsdk-tujtr-198a71e00a.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setDatabaseUrl("https://chefapp-eeae0.firebaseio.com")
+                .build();
+
+        FirebaseApp.initializeApp(options);
+
         return getAllRecommendations(chefId).size();
     }
 
